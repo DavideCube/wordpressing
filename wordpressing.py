71aaa4175bdd4e3f23051492f7cddac6f5e1f1bb
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2019-05-22 09:28:02 +0200

    introduction

--- wordpressing.py
+++ wordpressing.py
@@ -1,4 +1,5 @@
-
 #Wordpress exploiting tool designed by DavideCube
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
-print("Welcome to Wordpressing!!")
+print("Welcome to the Wordpressing Exploiting Tool!!")
+print("The tool will check if the available exploits are applicable to the wordpress site you provide.")
+print("Please provide a wordpress url: ")
