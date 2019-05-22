4cb7bdb3fab4019c6a00f5669ad135e8d4c54eb8
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2019-05-22 17:07:11 +0200

    starting loading the website

--- wordpressing.py
+++ wordpressing.py
@@ -1,5 +1,10 @@
 #Wordpress exploiting tool designed by DavideCube
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
+import BeautifulSoup
+import re
+import urllib
+
+print("\n")
 print("******************************************************************")
 print("*                                                                *")
 print("*         Welcome to the Wordpressing Exploiting Tool!!          *")
@@ -8,7 +13,15 @@ print("******************************************************************")
 print("*         The tool will check if the available exploits          *")
 print("*         are applicable to the wordpress site you provide.      *")
 print("******************************************************************")
-
+print("\n")
 url = input("Please provide a wordpress url:")
 
 print("The target is", url)
+
+data = urllib.urlopen(url).read() #read the content of the webpage
+
+#extract somehow the version tag
+
+#soup = BeautifulSoup.BeautifulSoup(data)
+#element = soup.find('meta', attrs={'name': 'generator', 'content': re.compile("^Wordpress.*")})
+#print(element.text)
