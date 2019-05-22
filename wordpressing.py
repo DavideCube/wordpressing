80a7e557b3a8c35bd116199cfe7e67fc88e76de9
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2019-05-22 10:09:58 +0200

    url input added

--- wordpressing.py
+++ wordpressing.py
@@ -2,4 +2,7 @@
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
 print("Welcome to the Wordpressing Exploiting Tool!!")
 print("The tool will check if the available exploits are applicable to the wordpress site you provide.")
-print("Please provide a wordpress url: ")
+
+url = input("Please provide a wordpress url:")
+
+print("The target is", url)
