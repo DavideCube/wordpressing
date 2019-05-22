946d0e02099b3c3f8859f2c440fc3ed6803cf9ab
CremoD <cremo.davide96@gmail.com>
2019-05-22 14:43:36 +0200

    test

--- wordpressing.py
+++ wordpressing.py
@@ -1,7 +1,13 @@
 #Wordpress exploiting tool designed by DavideCube
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
-print("Welcome to the Wordpressing Exploiting Tool!!")
-print("The tool will check if the available exploits are applicable to the wordpress site you provide.")
+print("******************************************************************")
+print("*                                                                *")
+print("*         Welcome to the Wordpressing Exploiting Tool!!          *")
+print("*                                                                *")
+print("******************************************************************")
+print("*         The tool will check if the available exploits          *")
+print("*         are applicable to the wordpress site you provide.      *")
+print("******************************************************************")
 
 url = input("Please provide a wordpress url:")
 
