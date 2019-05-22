bd89a11591670c8773b7472ac2602dbecb0ce0ff
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2019-05-22 09:16:20 +0200

    test

--- wordpressing.py
+++ wordpressing.py
@@ -1,4 +1,4 @@
 
 #Wordpress exploiting tool designed by DavideCube
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
-print "Welcome to Wordpressing"
+print("Welcome to Wordpressing")
