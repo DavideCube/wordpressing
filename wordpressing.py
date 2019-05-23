c7b3f0ff7841a43bac608fc09c4c472681cb38db
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2019-05-23 15:36:40 +0200

    Components versions check added

--- wordpressing.py
+++ wordpressing.py
@@ -1,9 +1,20 @@
 #Wordpress exploiting tool designed by DavideCube
 #Works on Wordpress version <= 4.5.3 and the WooCommerce Plugin version
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
-import urllib
+import urllib.request
+from modules import Exploit
 
+
+#create the exploit list, to be used later
+my_list = []
+
+my_list.append(Exploit.Exploit('dos', 'Simple DoS Attack', 1)) #only on WordPress 4.5.3
+my_list.append(Exploit.Exploit('xss', 'XSS attack', 1)) #only on WordPress 4.5.3
+my_list.append(Exploit.Exploit('xss+dos', 'XSS with Dos', 1)) #only on WordPress 4.5.3
+my_list.append(Exploit.Exploit('rce', 'WooCommerce RCE', 1)) #only on WooCommerce .... 
+
+#print main menu
 print("\n")
 print("******************************************************************")
 print("*                                                                *")
@@ -14,14 +25,32 @@ print("*         The tool will check if the available exploits          *")
 print("*         are applicable to the wordpress site you provide.      *")
 print("******************************************************************")
 print("\n")
-url = input("Please provide a wordpress url:")
+url = input("Please provide a complete wordpress url: ")
 
 print("The target is", url)
 
-data = urllib.urlopen(url).read() #read the content of the webpage
+data = urllib.request.urlopen(url).read() #read the content of the webpage
 
-#extract somehow the version tag
+soup = BeautifulSoup(data, 'html.parser') #parse the html code
 
-#soup = BeautifulSoup.BeautifulSoup(data)
-#element = soup.find('meta', attrs={'name': 'generator', 'content': re.compile("^Wordpress.*")})
-#print(element.text)
+#Check if the Wordpress version is the correct one
+element = soup.find('meta', attrs={'name':'generator','content':'WordPress 4.5.3'})
+
+if element is None:
+	print("The target site is not based on Wordpress 4.5.3")
+	my_list[0].executable = 0
+	my_list[1].executable = 0
+	my_list[2].executable = 0
+
+#now check if we can execute the Woocommerce RCE
+element =  soup.find('meta', attrs={'name':'generator','content':'WooCommerce 3.0.0'})
+
+if element is None:
+	print("The target site does not have WooCommerce 3.0.0")
+	my_list[3].executable = 0
+
+for i in range(len(my_list)): 
+	if my_list[i].executable == 1:
+		print( (i+1), ") ", my_list[i].desc)
+
+print("here is your checkmark: " + u'\u2713');
